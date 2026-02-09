cecf9bcda0fb59e263a95e166ffe015ef93362c6
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2026-02-09 16:28:07 +0100

    dev

--- addons/muk_web_chatter/__manifest__.py
+++ addons/muk_web_chatter/__manifest__.py
@@ -5,7 +5,7 @@
         This module improves the design of the chatter and adds a user
         preference to set the position of the chatter in the form view.
     ''',
-    'version': '19.0.1.2.4',
+    'version': '19.0.1.4.0',
     'category': 'Tools/UI',
     'license': 'LGPL-3', 
     'author': 'MuK IT',
@@ -36,6 +36,16 @@
                 'after', 
                 'mail/static/src/chatter/web_portal/chatter.js', 
                 'muk_web_chatter/static/src/chatter/chatter.js'
+            ),
+            (
+                'after',
+                'mail/static/src/core/common/composer.js',
+                'muk_web_chatter/static/src/chatter/composer.js'
+            ),
+            (
+                'after',
+                'mail/static/src/core/common/store_service.js',
+                'muk_web_chatter/static/src/chatter/store_service.js'
             ),
             (
                 'after', 
